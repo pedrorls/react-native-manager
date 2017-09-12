ff750809929597238bf7ab61fb36fa72067e880f
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-09-12 00:41:00 -0300

    Common components included tin project

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
         };
 
         firebase.initializeApp(config);
-        
+
     }
 
     render(){
@@ -35,4 +35,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default App; 
