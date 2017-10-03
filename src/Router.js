f4a217d888ba5a342baa05893234ca380308b55f
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-10-03 20:34:51 -0300

    navigation through app completed

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import {Scene, Router} from 'react-native-router-flux';
+import {Scene, Router, Actions} from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+
 
 const RouterComponent = () => {
     return(
-        <Router style={styles.sceneStyle}>
-            <Scene 
-                key='login'
-                component={LoginForm}
-                title='Login'
-                initial
-            />
-            <Scene
-                key='employeelist'
-                component={EmployeeList}
-                title='Employees'
-            />
+        <Router>
+            <Scene key='auth'>
+                <Scene key='login' component={LoginForm} title='Login' style={styles.sceneStyle}/>
+            </Scene>
+            <Scene key='main'>
+                <Scene
+                    onRight={() => Actions.employeeCreate()}
+                    rightTitle='Add' 
+                    key='employeesList'
+                    component={EmployeeList} 
+                    title='Employees'
+                    style={styles.sceneStyle}
+                    initial
+                />
+                <Scene
+                    key='employeeCreate'
+                    component={EmployeeCreate}
+                    title='New Employee'
+                    style={styles.sceneStyle}
+                />
+            </Scene>
         </Router>
     );
 };
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import {Actions} from 'react-native-react-flux';
+import {Actions} from 'react-native-router-flux';
 import {
     EMAIL_CHANGED,
     PASS_CHANGED,
@@ -41,6 +41,7 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS,
         payload: user
     });
+    Actions.main();
 };
 
 const loginUserFail =(dispatch) => {
