576ee1602bfeb838c33b8228906a262ffae3e2d7
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-10-05 20:06:24 -0300

    employee component is connected to employee action

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {employeeUpdate} from '../actions';
 import {Card, CardSection, Input, Button} from './common';
 
 
@@ -10,12 +12,17 @@ class EmployeeCreate extends Component {
                     <Input
                         label='Name'
                         placeholder='Name'
+                        value={this.props.name}
+                        onChangeText={value => this.props.employeeUpdate({prop: 'name', value})}
                     />
                 </CardSection>
                 <CardSection>
                     <Input
                         label='Phone'
                         placeholder='01234 5678'
+                        value={this.props.phone}
+                        type='numeric'
+                        onChangeText={value => this.props.employeeUpdate({prop: 'phone', value})}
                     />
                 </CardSection>
                 <CardSection>
@@ -28,4 +35,9 @@ class EmployeeCreate extends Component {
     }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+    const {name, phone, shift} = state.employeeForm;
+    return {name, phone, shift}
+};
+
+export default connect(mapStateToProps, {employeeUpdate})(EmployeeCreate);
